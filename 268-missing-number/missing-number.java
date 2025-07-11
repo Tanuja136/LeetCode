0efc60fa1dadd4a9aea9f18ed7661bbf40d9c4e0
TANUJA MALLIPAMU <119398635+Tanuja136@users.noreply.github.com>
2025-07-11 10:21:03 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (93.50%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedsum = n * (n + 1) / 2;
+        int sum = 0;
+        for(int num : nums) {
+            sum += num;
+        }
+        return expectedsum - sum;
+    }
+}
